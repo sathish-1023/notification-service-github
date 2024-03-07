f63f4b5daf7cd656958a9486dccce9b93f84b963
Sathish Projects <kalivimarsa705@gmail.com>
2024-03-07 15:52:25 +0530

    first commit

---
+++ src/main/java/com/mrParashurama/Notification_Service/NotificationServiceApplication.java
@@ -0,0 +1,26 @@
+package com.mrParashurama.Notification_Service;
+import com.mrParashurama.Notification_Service.dto.OrderPlacedEvent;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.KafkaListener;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@Slf4j
+@EnableKafka
+public class NotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationServiceApplication.class, args);
+	}
+
+
+	@KafkaListener(topics = "notificationTopic")
+	public void handleNotification(OrderPlacedEvent orderPlacedEvent){
+		log.info("Recieved Notification order is : {} ",orderPlacedEvent.getOrderNumber());
+	}
+
+}
